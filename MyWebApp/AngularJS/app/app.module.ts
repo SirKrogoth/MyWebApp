de16de59b5672e8f4093482d1aa19ba43a2a1434
João Rafael Fagundes Menezes <joaorafael652@outlook.com>
2019-01-22 00:14:45 -0200

    Foi inserido de forma satisfatória o Angular JS junto ao .NET Core 2.0.

---
+++ MyWebApp/AngularJS/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+      RouterModule.forRoot([], { useHash: true, enableTracing: true })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
